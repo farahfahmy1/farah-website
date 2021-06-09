c3e982439010633603b6f813714173bfadfbf11f
farahfahmy1 <85620035+farahfahmy1@users.noreply.github.com>
2021-06-09 04:16:56 -0700

    upload first file

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var getUserName = prompt("Hello! who are you? please provide you name: ");
+
+console.log(getUserName);
+
+document.getElementById("no").innerText = getUserName;
+
+// toogle 
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  }); 
+  
+
